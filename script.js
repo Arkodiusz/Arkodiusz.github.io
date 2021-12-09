92134c70fbfaf5911881bea61c76a4282c6ae5ea
Arkodiusz <arecki358@gmail.com>
2021-12-09 23:55:01 +0100

    tasker app

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 
 const cookbookWakeUrl = 'https://bcookbook.herokuapp.com/wakeup'
+const taskerWakeUrl = 'https://btasker.herokuapp.com/wakeup'
 
 window.onload = async (event) => {
-    fetch(cookbookWakeUrl)
+    await fetch(cookbookWakeUrl)
+        .then((response) => response.json())
+        .then((msg) => console.log('message =>', msg))
+
+    await fetch(taskerWakeUrl)
         .then((response) => response.json())
         .then((msg) => console.log('message =>', msg))
 };
