9cc10d1599517dbff4149f59b1738780d2b1d534
Arkodiusz <arecki358@gmail.com>
2021-12-12 23:09:24 +0100

    gender predictor wake-up

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 
 const cookbookWakeUrl = 'https://bcookbook.herokuapp.com/wakeup'
 const taskerWakeUrl = 'https://btasker.herokuapp.com/wakeup'
+const genderPredictorWakeUrl = 'https://bgenderator.herokuapp.com/'
 
-window.onload = async (event) => {
+window.onload = async () => {
     await fetch(cookbookWakeUrl)
         .then((response) => response.json())
         .then((msg) => console.log('message =>', msg))
@@ -10,4 +11,11 @@ window.onload = async (event) => {
     await fetch(taskerWakeUrl)
         .then((response) => response.json())
         .then((msg) => console.log('message =>', msg))
+
+    await fetch(genderPredictorWakeUrl)
+        .then((response) => {
+            response.status === 200
+                ? console.log('message =>', 'Gender Predictor app is awake')
+                : console.log('message =>', 'Error during waking up Gender Predictor')
+        })
 };
